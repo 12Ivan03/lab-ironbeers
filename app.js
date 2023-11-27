ead12525eb83cd2d3d37226a5342ec6995aee599
Pencho Ivanov <penchoivanov@Penchos-Air.home>
2023-11-27 19:06:57 +0100

    beers-page

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const port = 3000;
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-const port = 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -20,15 +20,21 @@ hbs.registerPartials(path.join(__dirname, "views/partials"));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index', {title: "home"});
+  res.render('index', {title: "home", bodyClass: "home-page"});
 });
 
 app.get('/beers', (req, res) => {
-  res.render('beers', {title: 'beers'});
+  punkAPI
+    .getBeers({})
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', {beersFromApi, bodyClass: "beer-page", title: "beers"});
+    })
+    .catch(error => console.log(error));
 });
 
 app.get('/random-beer', (req, res) => {
-  res.render('random-beer',{title: "random-beer" });
+  res.render('random-beer',{title: "random-beer", bodyClass: "random-beer-page" });
 });
 
 app.listen(port, () => console.log(`🏃‍ on port ${port}`));
