8cc4b54b799501bad734e84c3dee44fe01590c5c
Pencho Ivanov <penchoivanov@Penchos-Air.home>
2023-11-28 14:47:46 +0100

    cleaner working space

--- app.js
+++ app.js
@@ -13,11 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
 hbs.registerPartials(path.join(__dirname, "views/partials"));
-// ... 
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index', {title: "home", bodyClass: "home-page"});
@@ -27,7 +23,6 @@ app.get('/beers', (req, res) => {
   punkAPI
     .getBeers()
     .then(beersFromApi => {
-      //console.log('Beers from the database: ', beersFromApi)
       res.render('beers', {beersFromApi, bodyClass: "beer-page", title: "beers"});
     })
     .catch(error => console.log(error));
@@ -37,13 +32,9 @@ app.get('/random-beer', (req, res) => {
   punkAPI
     .getRandom()
     .then(randomBeerFromAPI => {
-      console.log('Beers from the database: ', randomBeerFromAPI)
       res.render('random-beer',{ randomBeerFromAPI, bodyClass: "random-beer-page", title: "random-beer" });
     })
-    .catch(error => {
-      console.error("Error fetching random beer:", error);
-      
-    });
+    .catch(error => console.error(error));
 });
 
-app.listen(port) //, () => console.log(`🏃‍ on port ${port}`));
+app.listen(port);
